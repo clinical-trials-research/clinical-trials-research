adbe8ba2bc3692b167e9b11a9b450bc1c22f293e
wuihee <wuihee@gmail.com>
2024-04-14 16:22:13 -0700

    Updated script to download fields

--- scripts/download_fields.py
+++ scripts/download_fields.py
@@ -7,12 +7,12 @@ GET_STUDIES_URL = API_URL + "/studies?pageSize=1000"
 GET_STUDY_SIZES_URL = API_URL + "/stats/size"
 
 
-def get_number_of_studies() -> int:
-    response_json = httpx.get(GET_STUDY_SIZES_URL).json()
-    return response_json.get("totalStudies")
+# Get number of pages.
+response_json = httpx.get(GET_STUDY_SIZES_URL).json()
+total_studies = response_json.get("totalStudies")
+pages = (total_studies // 1000) + 1
 
-
-pages = (get_number_of_studies() // 1000) + 1
+# Download fields.
 response_json = httpx.get(GET_STUDIES_URL).json()
 df = pd.json_normalize(response_json.get("studies"))
 
@@ -20,7 +20,9 @@ for _ in tqdm.trange(pages):
     try:
         next_page_token = response_json.get("nextPageToken")
         if next_page_token:
-            response_json = httpx.get(API_URL + f"&pageToken={next_page_token}").json()
+            response_json = httpx.get(
+                GET_STUDIES_URL + f"&pageToken={next_page_token}"
+            ).json()
             df = pd.concat(
                 [df, pd.json_normalize(response_json["studies"])], ignore_index=True
             )
