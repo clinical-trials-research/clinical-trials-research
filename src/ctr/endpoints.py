776d0c30645f7a6e1a2677ffe7aa81f9250cdfb7
anushkakg <112725991+anushkakg@users.noreply.github.com>
2024-06-25 18:34:37 -0700

    Update endpoints.py

--- src/ctr/endpoints.py
+++ src/ctr/endpoints.py
@@ -6,7 +6,7 @@ class StudiesEndpoints:
     Endpoints related to clinical trial studies.
     """
 
-    STUDIES = API + "/studies"
+    STUDIES = API + "/studies?query.cond=cancer+OR+cancers+OR+cancerous+OR+tumor+OR+carcinoma+OR+carcinogen+OR+chemo+OR+chemotherapy+OR+chrondosarcoma+OR+adenocarcinoma+OR+adenoma+OR+lymphoma+OR+leiomyoma+OR+leukemia+OR+lumpectomy+OR+macroglobulinemia+OR+melanoma+OR+metastasis+OR+micrometastases+OR+metastatic+OR+metastasize+OR+myeloma+OR+oncogenes+OR+oncologist+OR+oncology+OR+osteosarcoma+OR+radiation+therapy+OR+sarcoma&query.term=AREA%5BCompletionDate%5DRANGE%5B2019-01-01%2CMAX%5D&countTotal=true&pageSize=1000"
 
 
 class StatsEndpoints:
